2c1dbd7f829d5de9f5de0cd2c2622a5fddedd60a
Max Rabin <max.rabin17@gmail.com>
2021-02-07 17:36:20 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,11 +1,4 @@
-def multiply(num1, num2)
-  answer = (num1 * num2)
-  return answer
+def say_hello(name = "Ruby programmer")
+  puts ("hello " + name + "!")
 end
-
-def divide(num1, num2)
-  answer = (num1 / num2)
-  return answer
-end
-
-puts multiply(3, 6)
+puts say_hello("my dude")
